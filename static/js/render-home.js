ad7bcd928223692e016ee06d3266cda16fc88ad3
brenda-daroz <brenda.daroz@gmail.com>
2022-05-17 16:12:40 +0200

    Fix image path

--- static/js/render-home.js
+++ static/js/render-home.js
@@ -5,13 +5,13 @@ function renderHome() {
         h('article.article__destaque',
             h('h2.destaques.right',
                 h('span', 'Destaques')),
-            h('img.img__destaque', { src: 'spa/static/images/cake-1194779_1280.jpeg' })),
+            h('img.img__destaque', { src: '/static/images/cake-1194779_1280.jpeg' })),
         h('article.article__latest',
             h('h2.latest.right',
                 h('span', 'Ultimos posts')),
-            h('img.img__latest', { src: '/spa/static/images/burger.jpeg' }),
+            h('img.img__latest', { src: '/static/images/burger.jpeg' }),
             h('p', 'Hamburguer'),
-            h('img.img__latest', { src: '/spa/static/images/cupcakes-5447570_1280.jpeg' }),
+            h('img.img__latest', { src: '/static/images/cupcakes-5447570_1280.jpeg' }),
             h('p', 'Cupcake'),
         ))
 
